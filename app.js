b8172af85717af1317b1cc2c908e465700e4011c
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-02 18:08:38 +0200

    Connection to development mongo database

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+// This is a developing database
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    userNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+// Checking if the connection is made
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, "Connection error"));
+db.once('open', () => {
+    console.log('Database connection established');
+})
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
