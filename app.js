9036e8eb3681d49580443d77d6638dfc37505c92
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-02 17:54:50 +0200

    npm install -g nodemon

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
+
