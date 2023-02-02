9e16f9861f0df51d4e38f721d5cfb2f8a62a261b
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-03 01:32:02 +0200

    app.js

--- app.js
+++ app.js
@@ -22,14 +22,33 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// parsing body data
+app.use(express.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home');
 })
-
+// Showing all campgrounds
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
 })
+// Adding a new campground page
+app.get('/campgrounds/new', (req, res) => {
+    res. render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+})
+
 
 // start with nodemon app.js
 app.listen(3000, () => {
