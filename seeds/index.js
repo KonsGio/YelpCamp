cf6ff0566088a5a3eb8bd04befb4f4d010c53359
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-02 20:28:43 +0200

    Creating 50 new cities with camp names and then closing the database

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
-
+const {places, descriptors} = require('./seedHelpers');
 require('dotenv').config();
 
 // This is a developing database
@@ -17,15 +17,20 @@ db.once('open', () => {
     console.log('Database connection established');
 })
 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
         });
         await camp.save();
     }
 };
 
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+})
