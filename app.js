32b84a816c3a7a7ece6621265c29489fa45d8628
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-02 19:26:06 +0200

    Adding new campsite in database

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+require('dotenv').config();
 
 // This is a developing database
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -23,6 +25,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home');
 })
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping'});
+    await camp.save();
+    res.send(camp);
+})
+
 // start with nodemon app.js
 app.listen(3000, () => {
     console.log('listening on port 3000');
