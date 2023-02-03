0fc6f373e2c15c1ed4a1b31c612a07820bfb7886
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-03 17:08:48 +0200

    Added ejs mate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 require('dotenv').config();
@@ -19,6 +20,8 @@ db.once('open', () => {
 })
 
 const app = express();
+
+app.engine('ejs', ejsMate);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
