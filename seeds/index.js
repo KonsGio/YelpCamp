118b316e03123f27eb9f7489e2a1d7df1ea669ea
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-03 22:53:38 +0200

    Generating campground with image desc and price

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) +10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptatem voluptate placeat itaque consequatur ipsum nam nulla similique temporibus, ipsa accusamus? Distinctio possimus numquam voluptates, veniam iusto ipsam quasi vitae.',
+            price
         });
         await camp.save();
     }
