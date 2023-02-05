7c3600bd8c94b6d6a4fcebab51ee046578ab4253
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-06 01:16:47 +0200

    404 error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 require('dotenv').config();
@@ -47,35 +48,50 @@ app.get('/campgrounds/new', (req, res) => {
     res. render('campgrounds/new')
 })
 
+//  --------------------------------
 app.post('/campgrounds', catchAsync (async (req, res, next) => {
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
-}));
+}))
 
+// Showing campground by id
 app.get('/campgrounds/:id', catchAsync (async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
 }))
 
+// Editing campground by id
 app.get('/campgrounds/:id/edit', catchAsync (async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground});
 }))
 
+// Updating campground by id
 app.put('/campgrounds/:id', catchAsync (async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
+// Deleting campground by id
 app.delete('/campgrounds/:id', catchAsync (async (req, res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }))
 
+// Passing a new ExpressError to next * means that all other error checks passed through ok
+// It means that campgrounds/anything is an error with 404 status
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
+})
+
+// Showing generic error message
 app.use((err, req, res, next) => {
+    // taking statusCode and message from catchAsync with default values
+    const {statusCode = 500, message = 'Something went wrong'} = err;
+    res.status(statusCode).send(message);
     res.send('Oh Boy something went wrong');
 });
 
