2eef1a6acae20a204a25eb42ec739a9716d215bb
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-06 01:20:35 +0200

    ERROR throwing fo incomplete new campground form

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.get('/campgrounds/new', (req, res) => {
 
 //  --------------------------------
 app.post('/campgrounds', catchAsync (async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Incomplete Campground Data', 400);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
