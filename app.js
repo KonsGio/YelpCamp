b0cabfc8f945fee756cf58ac727bf588346ad85e
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-06 02:06:20 +0200

    Passing  an error message, JOI Schema

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -50,7 +51,22 @@ app.get('/campgrounds/new', (req, res) => {
 
 //  --------------------------------
 app.post('/campgrounds', catchAsync (async (req, res, next) => {
-    if(!req.body.campground) throw new ExpressError('Incomplete Campground Data', 400);
+    // if(!req.body.campground) throw new ExpressError('Incomplete Campground Data', 400);
+        const campgroundSchema = Joi.object({
+            campground: Joi.object({
+                title: Joi.string().required(),
+                price: Joi.number().required().min(0),
+                image: Joi.string().required(),
+                location: Joi.string().required(),
+                description: Joi.string().required()
+            }).required()
+        }) 
+        const {error} = campgroundSchema.validate(req.body);
+        if(error){
+            const msg = error.details.map(el => el.message).join(',')
+            throw new ExpressError(msg, 400)
+        }
+        console.log(result);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
