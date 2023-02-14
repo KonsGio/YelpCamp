82a4c8541e63264692553074c7a3e3fe2b6ff628
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-14 15:21:24 +0100

    HttpOnly set to true for XMS security

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     cookie:{
+        httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
