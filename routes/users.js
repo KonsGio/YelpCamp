c7ac9b70620ac5a858555930b4c1832966eadbe8
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-15 15:25:41 +0100

    Try and catch already registered user

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const { models } = require('mongoose');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    const {email, username, password} = req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash('Welcome to YelpCamp');
-    res.redirect('/campgrounds');
-});
+router.post('/register', catchAsync (async (req, res) => {
+    try{
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to YelpCamp');
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+
+}));
 
 module.exports = router;
