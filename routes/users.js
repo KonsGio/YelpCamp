12a9f8354325f2020c808cec34b91b5456c8d1cd
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-15 15:20:33 +0100

    Registering User to MongoDB

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,12 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-    res.send(req.body);
+    const {email, username, password} = req.body;
+    const user = new User({email, username});
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash('Welcome to YelpCamp');
+    res.redirect('/campgrounds');
 });
 
 module.exports = router;
