7a4e39570113bc149ea45fc262489c62b3847fb9
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-16 13:23:12 +0100

    Moving functions to Reviews controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,32 +3,15 @@ const express = require("express");
 // in app.js and granting access to review campground id
 const router = express.Router({mergeParams: true});
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
-const Review = require('../models/review');
+const campgrounds = require('../controllers/reviews');
 const { isLoggedIn, validateReview, isReviewAuthor } = require("../middleware");
 
 
 
 // Submitting the review form to this url
-router.post('/', isLoggedIn, validateReview, catchAsync (async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success','Thank you for your review!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync (campgrounds.createReview))
 
 // Deleting reviews by id
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    // using mongo $pull to remove review associated with specific id
-    const {id, reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: { review: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success','Your review has been deleted!')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(campgrounds.deleteReview))
 
 module.exports = router;
