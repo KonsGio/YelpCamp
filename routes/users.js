c595ed0fbecb2f93d13beeb817d8f970ef22972c
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-02-16 13:50:55 +0100

    Grouping users routes

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,18 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync (users.register))
 
-router.post('/register', catchAsync (users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', {
-    failureFlash: true, 
-    failureRedirect: '/login', 
-    keepSessionInfo: true }), 
-    users.login
-)
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {
+        failureFlash: true, 
+        failureRedirect: '/login', 
+        keepSessionInfo: true }), 
+        users.login
+    )
 
 router.get('/logout', users.logout);
 
